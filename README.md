f58a4b0a5e64f2affaba42847de345f964295a6e
Esteban Olguin <olguinesteban20@outlook.com>
2023-06-14 11:14:46 -0300

    Subida del repositorio

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dropdown Menu
+
+- Dropdown Menu Using HTML CSS SASS & JavaScript
+- Contains a button that shows and hides the menu.
+- Along with an animation when showing the menu.
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*=============== DROPDOWN JS ===============*/
+const showDropdown = (content, button) => {
+    const dropdownContent = document.getElementById(content),
+        dropdownButton = document.getElementById(button);
+
+        dropdownButton.addEventListener('click', () => {
+            /* Add the show-dropdown class, so that the menu is displayed */
+            dropdownContent.classList.toggle('show-dropdown');
+        })
+};
+
+showDropdown('dropdown-content', 'dropdown-button');
